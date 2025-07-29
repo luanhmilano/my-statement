f5e601ccc28369a968fd9243861bce8f83428c98
Luan H. S. Silva <luanhsantos14@gmail.com>
2025-07-29 10:36:56 -0300

    feat(createAccountPage): add validation and style for errors

--- src/pages/CreateAccount/components/CreateAccountForm.tsx
+++ src/pages/CreateAccount/components/CreateAccountForm.tsx
@@ -1,8 +1,19 @@
+import { useForm } from 'react-hook-form';
 import styles from '../styles/create-account-form.module.css';
+import { createAccountSchema, type CreateAccountData } from '../../../schemas/createAccountSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 export default function CreateAccountForm() {
+  const { register, handleSubmit, formState: { errors } } = useForm<CreateAccountData>({
+    resolver: zodResolver(createAccountSchema),
+  });
+
+  const onSubmit = (data: CreateAccountData) => {
+    console.log("Form data: ", data);
+  }
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)} noValidate>
       <h1 className={styles.title}>Create account</h1>
       <p className={styles.subtitle}>
         Please fill in the fields below to create an account.
@@ -11,47 +22,68 @@ export default function CreateAccountForm() {
       <div className={styles.row}>
         <div className={styles.fieldColumn}>
           <label className={styles.label}>Last Name</label>
-          <input
-            type="text"
-            className={styles.input}
-            placeholder="Enter your last name"
-          />
+          <span className={styles.inputContainer}>
+            <input
+              type="text"
+              className={styles.input}
+              placeholder="Enter your last name"
+              {...register("lastName")}
+            />
+            {errors.lastName && <span className={styles.error}>{errors.lastName.message}</span>}
+          </span>
         </div>
+
         <div className={styles.fieldColumn}>
           <label className={styles.label}>First Name</label>
-          <input
-            type="text"
-            className={styles.input}
-            placeholder="Enter your first name"
-          />
+          <span className={styles.inputContainer}>
+            <input
+              type="text"
+              className={styles.input}
+              placeholder="Enter your first name"
+              {...register("firstName")}
+            />
+            {errors.firstName && <span className={styles.error}>{errors.firstName.message}</span>}
+          </span>
         </div>
       </div>
 
       <div className={styles.field}>
         <label className={styles.label}>E-mail</label>
-        <input
-          type="email"
-          className={styles.input}
-          placeholder="Enter your e-mail"
-        />
+        <span className={styles.inputContainer}>
+          <input
+            type="email"
+            className={styles.input}
+            placeholder="Enter your e-mail"
+            {...register("email")}
+          />
+          {errors.email && <span className={styles.error}>{errors.email.message}</span>}
+        </span>
       </div>
 
       <div className={styles.field}>
         <label className={styles.label}>Password</label>
-        <input
-          type="password"
-          className={styles.input}
-          placeholder="Enter your password"
-        />
+        <span className={styles.inputContainer}>
+          <input
+            type="password"
+            className={styles.input}
+            placeholder="Enter your password"
+            {...register("password")}
+          />
+          {errors.password && <span className={styles.error}>{errors.password.message}</span>}
+        </span>
       </div>
 
       <div className={styles.field}>
         <label className={styles.label}>Confirm Password</label>
-        <input
-          type="password"
-          className={styles.input}
-          placeholder="Confirm your password"
-        />
+        <span className={styles.inputContainer}>
+          <input
+            type="password"
+            className={styles.input}
+            placeholder="Confirm your password"
+            {...register("confirmPassword")}
+          />
+          {errors.confirmPassword && <span className={styles.error}>{errors.confirmPassword.message}</span>}
+        </span>
       </div>
 
       <button type="submit" className={styles.button}>
